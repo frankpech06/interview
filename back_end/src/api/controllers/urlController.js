fa93cf4f59132b986bb48ce10d48d0c83baa5e6e
unknown <Administrador@MXCRVP3LP05.pricetravel.com.mx>
2020-11-23 00:07:22 -0500

    The method to retrieve an original URL and redirect the response is implemented. Also the method is called in the corresponding route

--- back_end/src/api/controllers/urlController.js
+++ back_end/src/api/controllers/urlController.js
@@ -1,7 +1,37 @@
+const mongoose      = require('mongoose');
+const { nanoid }    = require('nanoid');
 const ResponseModel = require('../../models/ResponseModel');
+const Url           = require('../../models/Url');
+const validUrl      = require('valid-url');
 
-const getFullUrl = async (next) => {
-    let rm = new ResponseModel();
+/**
+ * Method that retrieves an original URL from the database to redirect the response
+ * @param  {string}     urlCode : Short URL code to get the original URL
+ * @param  {Function}   next    : Function to execute after the database searching
+ */
+const getFullUrl = async (urlCode, next) => {
+    let rm          = new ResponseModel();
+    let urlObject   = null;
+
+    try{
+        // Checking if there is any URL associated to the url code
+        urlObject = await Url.findOne({shortUrlCode: urlCode});
+        if (urlObject){
+            // Storing the original URL in the response object
+            rm.data['urlToRedirect']= urlObject.originalUrl;
+        }else{
+            // If there is no a URL in the database, an error is returned
+            rm.error        = true;
+            rm.message      = "Url not found.";
+            rm.statusCode   = 404;
+        }
+    }catch(err){
+        // If there is any error, an error is returned
+        rm.error        = true;
+        rm.message      = "Url not found.";
+        rm.statusCode   = 404;
+    }
+    
     next(rm);
 }
 
--- back_end/src/api/routes/urlRoutes.js
+++ back_end/src/api/routes/urlRoutes.js
@@ -9,11 +9,21 @@ const urlController = require('../controllers/urlController'); // Retrieving the
  * @returns JSON response with the search result
  */
 router.get('/:shortUrlCode', (req, res) => {
-    urlController.getFullUrl((rm) => {
-        res.status(rm.statusCode).json({
-            message: rm.message,
-            data: rm.data
-        });
+    // Getting the short URL code from the request param
+    let shortUrlCode = req.params.shortUrlCode;
+
+    // Calling to the controller
+    urlController.getFullUrl(shortUrlCode, (rm) => {
+        if(rm.error){
+            // If there is an error, the message is sent in the response
+            res.status(rm.statusCode).json({
+                message: rm.message,
+                data: rm.data
+            });
+        }else{
+            // If there is not an error, a redirect is sent
+            res.redirect(rm.data['urlToRedirect'])
+        }
     });
 });
 
